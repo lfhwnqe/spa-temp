fbf720ed3192c40e85535c57f56303ca748e4b79
lfhwnqe <lfhwnqea@gmail.com>
2024-11-11 23:48:18 +0800

    feature: 补充上次提交漏掉的依赖

--- src/pages/todo/todo.tsx
+++ src/pages/todo/todo.tsx
@@ -4,19 +4,26 @@ import { Provider } from "jotai";
 import TodoList from "./todoList";
 import TodoInput from "./todoInput";
 import TodoFilter from "./todoFilter";
+import { Helmet } from "react-helmet-async";
 
 const Todo: React.FC = () => {
+  
   return (
-    <div className="min-h-screen bg-gray-100 py-8">
-      <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
-        <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
-          Todo App
-        </h1>
-        <TodoInput />
-        <TodoFilter />
-        <TodoList />
+    <>
+      <Helmet>
+        <title>待办事项</title>
+      </Helmet>
+      <div className="min-h-screen bg-gray-100 py-8">
+        <div className="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
+          <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
+            Todo App
+          </h1>
+          <TodoInput />
+          <TodoFilter />
+          <TodoList />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 Todo.whyDidYouRender = true;
